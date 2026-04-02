9952b85f322b63441c0201b239671c7cb7b4d169
Andrei Udalov <137709149+AndreiUdalovIT@users.noreply.github.com>
2026-04-03 00:36:52 +0300

    init

---
+++ index.js
@@ -0,0 +1,60 @@
+const input = document.getElementById('input__text');
+const btn = document.getElementById('input__btn');
+const listMovies = document.getElementById('movies');
+const movieInfo = document.getElementById('movie-info');
+
+const baseURL = 'http://www.omdbapi.com/?apikey=76f371c1&';
+const search = 's=';
+const searchID = 'i=';
+
+
+const getInputValue = () => {
+    const inputValue = input.value;
+    return inputValue;
+}
+
+const changeInput = () => {
+    listMovies.innerHTML = '';
+}
+
+const renderList = (element) => {
+    const newElementLI = document.createElement('li');
+    newElementLI.innerHTML = `<a class="movies__item" href="movie.html?id=${element.imdbID}">
+                    <img class="movies__img" src="${element.Poster}" alt="Фото фильма">
+                    <h2 class="movies__name">${element.Title}</h2>
+                    <p class="movies__year">${element.Year}</p>
+                    <p class="movies__category">${element.Type}</p>
+                </a>`
+    listMovies.appendChild(newElementLI);
+}
+
+const getID = (event) => {
+    const item = event.target.closest('.movies__item');
+    if (!item) {
+        return null;
+    }
+    const id = item.id;
+    return id;
+}
+
+const clickOnMovie = (event) => {
+    const id = getID(event);
+    if (!id) return;
+    window.location.href = `movie.html?id=${id}`;
+}
+
+const clickBtnInput = () => {
+    fetch(`${baseURL}${search}${getInputValue()}`)
+    .then(data => data.json())
+    .then((response) => {
+        response.Search.forEach(element => {
+            renderList(element);
+        });
+    })
+    input.addEventListener('input', changeInput, { once: true })
+}
+
+listMovies.addEventListener('click', clickOnMovie);
+btn.addEventListener('click', clickBtnInput);
+
+
---
+++ movie.js
@@ -0,0 +1,40 @@
+const movieInfo = document.getElementById('movie-info');
+
+const params = new URLSearchParams(window.location.search);
+const id = params.get('id');
+
+const baseURL = 'http://www.omdbapi.com/?apikey=76f371c1&';
+const searchID = 'i=';
+
+const renderInfo = (response) => {
+    const newElementDIV = document.createElement('div');
+    newElementDIV.innerHTML = `<div class="id-movie__flex">
+                <div class="id-movie__left">
+                    <img class="id-movie__left__img" src="${response.Poster}" alt="Фото фильма">
+                </div>
+                <div class="id-movie__right">
+                    <h2 class="id-movie__right__name">${response.Title}</h2>
+                    <ul class="id-movie__right__flex">
+                        <li class="text-parameter">Год:<span class="text-info">    ${response.Year}</span></li>
+                        <li class="text-parameter">Рейтинг:<span class="text-info">    ${response.Rated}</span></li>
+                        <li class="text-parameter">Дата выхода:<span class="text-info">    ${response.Released}</span></li>
+                        <li class="text-parameter">Продолжительность:<span class="text-info">    ${response.Runtime}</span></li>
+                        <li class="text-parameter">Жанр:<span class="text-info">    ${response.Genre}</span></li>
+                        <li class="text-parameter">Режиссер:<span class="text-info">    ${response.Director}</span></li>
+                        <li class="text-parameter">Сценарий:<span class="text-info">    ${response.Writer}</span></li>
+                        <li class="text-parameter">Актеры:<span class="text-info">    ${response.Actors}</span></li>
+                        <li class="text-parameter">Страна:<span class="text-info">    ${response.Country}</span></li>
+                    </ul>
+                </div>
+            </div>
+            <p class="text-about">${response.Plot}</p>`
+    movieInfo.appendChild(newElementDIV);
+}
+
+const getInfoAndRender = () => {
+    fetch(`${baseURL}${searchID}${id}`)
+    .then(data => data.json())
+    .then(response => renderInfo(response)) 
+}
+
+getInfoAndRender();
